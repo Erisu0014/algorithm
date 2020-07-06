cbb753774e100edfb9089c4e34744d2134b541ed
alice <1269732086@qq.com>
2020-07-06 17:24:49 +0800

    没劲

--- src/Sum.java
+++ src/Sum.java
@@ -99,8 +99,42 @@ public class Sum {
         return sum;
     }
 
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        /**
+         * @Description //63. 不同路径 II https://leetcode-cn.com/problems/unique-paths-ii/
+         * @Date  2020/7/6 16:42
+         * @Param  obstacleGrid
+         * @return int
+         **/
+        if (obstacleGrid == null || obstacleGrid.length == 0) {
+            return 0;
+        }
+
+        int [][]dp=new int[obstacleGrid.length][obstacleGrid[0].length];
+
+        for (int i=0;i<obstacleGrid[0].length&&obstacleGrid[0][i]==0;i++){
+            dp[0][i]=1;
+        }
+        for (int i=0;i<obstacleGrid.length&&obstacleGrid[i][0]==0;i++){
+            dp[i][0]=1;
+        }
+        for (int i=1;i<obstacleGrid.length;i++){
+            for (int j=1;j<obstacleGrid[0].length;j++){
+                if(obstacleGrid[i][j] == 0)
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
+
+    }
+
 
     public static void main(String[] args) {
+
+        int [][]obstacleGrid=new int[1][2];
+        obstacleGrid[0][0]=1;
+        obstacleGrid[0][1]=0;
+        new Sum().uniquePathsWithObstacles(obstacleGrid);
 //        int num = new Sum().threeSumClosest(new int[]{-1, 2, 1, -4}, 1);
 //        System.out.println(num);
 
