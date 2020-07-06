8788e5804588daa246ac9f2d4dd1ecf5196ed1bc
alice <1269732086@qq.com>
2020-07-06 17:59:10 +0800

    没劲*2

--- src/Match.java
+++ src/Match.java
@@ -104,11 +104,38 @@ public class Match {
 
     }
 
+    public boolean isMatch(String s, String p) {
+        /**
+         * @Description // 44. 通配符匹配 https://leetcode-cn.com/problems/wildcard-matching/
+         * @Date  2020/7/6 17:34
+         * @Param s 字符串
+         * @param p 字符模式
+         * @return boolean
+         **/
+        boolean [][]dp=new boolean[s.length()+1][p.length()+1];
+
+        dp[0][0]=true;
+        for (int i = 1; i <= p.length(); ++i) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+        for (int i=1;i<=s.length();i++){
+            for (int j=1;j<=p.length();j++){
+                if (s.charAt(i-1)==p.charAt(j-1)||p.charAt(j-1)=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }else if (p.charAt(j-1)=='*'){
+                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+
     public static void main(String[] args) {
 //        System.out.println(new Match().patternMatching("bbb", "xxxxxx"));
-        List<String> s = new ArrayList<>();
-        s.add("leet");
-        s.add("code");
-        System.out.println(new Match().wordBreak("leetcode", s));
+
     }
 }
